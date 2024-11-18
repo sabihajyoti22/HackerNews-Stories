053bf6b3addb222d85c91a6b7c58f274612c980b
Sabiha Nasrin Jyoti <sabihajyoti@gmail.com>
2024-11-18 11:39:54 +0600

    chore: remove unused codes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ const app = createApp(App)
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
 
-app.config.globalProperties.$axios = axios
 app.use(router)
 app.use(pinia)
 app.use(plugin)
