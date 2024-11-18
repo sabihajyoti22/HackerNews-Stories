a911fa14a16b0c4762d54a14a717dcedeae9c0ff
Sabiha Nasrin Jyoti <sabihajyoti@gmail.com>
2024-11-18 09:15:09 +0600

    feat: add loader

---
+++ src/components/utils/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="min-w-screen min-h-[80vh] flex justify-center items-center">
+    <div
+      :style="{height: height+'px', width: width+'px'}"
+      class="border-8 border-paragraph200 rounded-full border-t-8 border-t-primary animate-spin"
+    />
+  </div>
+</template>
+<script lang="ts">
+  export default{
+    props: {
+      height: {
+        type: Number,
+        default: 60
+      },
+      width: {
+        type: Number,
+        default: 60
+      }
+    }
+  }
+</script>
